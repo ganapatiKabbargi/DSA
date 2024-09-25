9106c4abffe707dbcfccd13fbb957e457d7415dd
ganapatiKabbargi <ganapatisk96@gmail.com>
2024-09-25 11:14:08 +0530

    bubble sort

---
+++ sorting.js
@@ -0,0 +1,13 @@
+// Bubble Sort
+for (let i = 0; i < arr.length - 1; i++) {
+  let swap = false;
+  for (let j = 0; j < arr.length - 1 - i; j++) {
+    if (arr[j] > arr[j + 1]) {
+      let temp = arr[j + 1];
+      arr[j + 1] = arr[j];
+      arr[j] = temp;
+      swap = true;
+    }
+  }
+  if (!swap) return;
+}
