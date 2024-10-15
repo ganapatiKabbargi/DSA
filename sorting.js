b114582f4306ce6b885b7627e3589fe2967e8c08
ganapatiKabbargi <ganapatisk96@gmail.com>
2024-10-15 12:24:41 +0530

    insertion sort

--- sorting.js
+++ sorting.js
@@ -24,3 +24,14 @@ for (let i = 0; i < arr.length; i++) {
   arr[smallest] = arr[i];
   arr[i] = temp;
 }
+
+// Insertion sort
+for (let i = 1; i < arr.length; i++) {
+  let current = arr[i];
+  let j = i - 1;
+  while (j >= 0 && arr[j] > current) {
+    arr[j + 1] = arr[j];
+    j--;
+  }
+  arr[j + 1] = current;
+}
